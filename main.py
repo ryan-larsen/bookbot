2c5499c69987573216c4c58ffe4e159a4654f065
nullSar <nullSar@proton.me>
2025-04-21 20:23:26 -0400

    print book

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ def get_book_text(x):
     return file_contents
 
 def main():
-    x = count()
-    print(f"{x} words found in the document")
-
-def count():
-    words = get_book_text("books/frankenstein.txt").split()
-    word_count = len(words)
-    return word_count
-
+  try:
+    print(get_book_text("books/frankenstein.txt"))
+  except FileNotFoundError:
+     print("File could not be found")
+     
 main()
